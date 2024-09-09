7ed43655871c8e176e29b1bd5a9c696a39920647
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-09-09 23:06:24 +0200

    misc: comments

--- src/chess/masks.h
+++ src/chess/masks.h
@@ -2,15 +2,18 @@
 
 #include "types.h"
 
+// masks.x
+// Various bitmasks
+
 void InitMasks(void);
 
-BB Ray(Square from, Square to);
-BB XRay(Square from, Square to);
-BB Ring(Square sq, int offset);
-BB BAB(Square sq, PieceType p);
-BB PawnAttacks(Square sq, Color color);
-BB KnightAttacks(Square sq);
-BB BishopAttacks(Square sq);
-BB RookAttacks(Square sq);
-BB KingAttacks(Square sq);
-BB Attacks(Square sq, PieceType p);
+BB Ray(Square from, Square to);         // Has bits set between "from" and "to", inclusive
+BB XRay(Square from, Square to);        // Has bits set from "to" onwards, exclusive
+BB Ring(Square sq, int offset);         // Has bits set in a ring around "sq"
+BB PawnAttacks(Square sq, Color color); // Denotes pawn attacks for an empty chess board
+BB KnightAttacks(Square sq);            // Denotes knight attacks for an empty chess board
+BB BishopAttacks(Square sq);            // Denotes bishop attacks for an empty chess board
+BB RookAttacks(Square sq);              // Denotes rook attacks for an empty chess board
+BB KingAttacks(Square sq);              // Denotes king attacks for an empty chess board
+BB Attacks(Square sq, PieceType p);     // Simply a switch between individual attack functions
+BB BAB(Square sq, PieceType p);         // Has bits set as "Attacks" excluding the edge
