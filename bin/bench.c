2402ef7a64f6aa8376031099c702f61018c8d9cb
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-09-23 13:29:45 +0200

    misc: more benchmarks

--- bin/bench.c
+++ bin/bench.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <time.h>
 
-#include "chess/attacks.h"
 #include "chess/board.h"
 #include "chess/perft.h"
 #include "misc.h"
@@ -37,6 +36,9 @@ int main(void) {
     printf("beginning benchmark\n");
     bench_perft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 6, 119060324);
     bench_perft("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ", 5, 193690690);
+    bench_perft("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ", 6, 11030083);
+    bench_perft("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 6, 706045033);
+    bench_perft("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ", 5, 89941194);
     const float m_nodes = total_nodes / 1000000.0f;
     printf("aggregate: %lld n %f s (%.0f M n/s)\n", total_nodes, total_time, m_nodes / total_time);
     Fini();
