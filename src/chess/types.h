146ab5759d8abca4b4e9169282e5c7f36b6c8376
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-09-25 12:01:27 +0200

    refactor: cleans types

--- src/chess/types.h
+++ src/chess/types.h
@@ -2,24 +2,21 @@
 
 #include <stdint.h>
 
-#define WIDTH 8
-#define HEIGHT 8
-#define SQUARE_COUNT (WIDTH * HEIGHT)
-
-#define COLOR_COUNT 2
-#define PIECE_COUNT 6
-
-#define MAX_PLY 128
-#define MAX_MOVES 256
+#define WIDTH        8
+#define HEIGHT       8
+#define COLOR_COUNT  2
+#define PIECE_COUNT  6
+#define SQUARE_COUNT 64
+#define MAX_PLY      128
+#define MAX_MOVES    256
 
 typedef uint64_t Hash;
 typedef uint64_t BB;
 typedef enum Color { WHITE, BLACK, COLOR_NONE } Color;
 typedef enum PieceType { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, PIECE_TYPE_NONE } PieceType;
 typedef enum Castling { CASTLING_NONE, CASTLING_KING, CASTLING_QUEEN, CASTLING_BOTH } Castling;
-
-extern const char *PIECE_CHARS[COLOR_COUNT];
-
+typedef enum Column { COL_A, COL_B, COL_C, COL_D, COL_E, COL_F, COL_G, COL_H, COL_NONE } Column;
+typedef enum Row { ROW_1, ROW_2, ROW_3, ROW_4, ROW_5, ROW_6, ROW_7, ROW_8, ROW_NONE } Row;
 // clang-format off
 typedef enum Square {
     A1,B1,C1,D1,E1,F1,G1,H1,
@@ -32,12 +29,10 @@ typedef enum Square {
     A8,B8,C8,D8,E8,F8,G8,H8,
     SQUARE_NONE
 } Square;
-extern const char *SQUARES[SQUARE_COUNT];
 // clang-format on
 
-typedef enum Column { COL_A, COL_B, COL_C, COL_D, COL_E, COL_F, COL_G, COL_H, COL_NONE } Column;
-
-typedef enum Row { ROW_1, ROW_2, ROW_3, ROW_4, ROW_5, ROW_6, ROW_7, ROW_8, ROW_NONE } Row;
+extern const char *PIECE_CHARS[COLOR_COUNT];
+extern const char *SQUARES[SQUARE_COUNT];
 
 BB ToBB(Square sq);
 Square SquareFrom(Column col, Row row);
