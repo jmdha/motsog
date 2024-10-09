ac61386f523adbfdd2cd838bbb0f3763166835b2
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-10-10 00:12:30 +0200

    refactor: use min

--- src/eval/eval.c
+++ src/eval/eval.c
@@ -1,9 +1,8 @@
 #include "eval.h"
-#include "bit.h"
-#include "values.h"
+#include "misc.h"
 
 int Evaluate(const Position *pos, Color side) {
-    const unsigned int phase_mg = (pos->phase > 24) ? 24 : pos->phase;
+    const unsigned int phase_mg = min(24u, pos->phase);
     const unsigned int phase_eg = 24 - phase_mg;
 
     const int score_mg = pos->eval_mg[side] - pos->eval_mg[1 - side];
