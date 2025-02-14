81c0ea3cef065aea3c370a831d9d8c5c0b850c0e
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-02-14 18:15:07 +0100

    fix: temp fix to remove timeouts
    Results of dev vs prod (1+0.01, NULL, NULL,UHO_Lichess_4852_v1.epd):
    Elo: -50.96 +/- 43.12, nElo: -57.19 +/- 47.44
    LOS: 0.91 %, DrawRatio: 31.07 %, PairsRatio: 0.54
    Games: 206, Wins: 66, Losses: 96, Draws: 44, Points: 88.0 (42.72 %)
    Ptnml(0-2): [21, 25, 32, 13, 12], WL/DD Ratio: 9.67
    LLR: -1.00 (-2.94, 2.94) [0.00, 10.00]
    --------------------------------------------------
    Started game 217 of 20000 (dev vs prod)^C
    Player: prod  Timeouts: 8  Crashed: 0

--- src/search/search.c
+++ src/search/search.c
@@ -100,7 +100,7 @@ Move find_best_move(const Position *pos, unsigned int time_limit) {
         move_print(best);
         printf("\n");
         fflush(stdout);
-        if (NODES == 1 || abs(val) == INT_MAX || t * 64 > time_limit)
+        if (NODES == 1 || abs(val) == INT_MAX || t * 256 > time_limit)
             break;
     }
     return best;
