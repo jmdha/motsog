ea7ea95cf273654225b6a9c08bc58daf33c69cfc
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-10-02 08:03:15 +0200

    refactor: update misc

--- src/bit.c
+++ src/bit.c
@@ -3,31 +3,31 @@
 #include "bit.h"
 
 int popcount(uint64_t v) { 
-    return __builtin_popcountll(v); 
+	return __builtin_popcountll(v); 
 }
 
 int lsb(uint64_t v) {
-    assert(v);
-    return __builtin_ctzll(v);
+	assert(v);
+	return __builtin_ctzll(v);
 }
 
 int msb(uint64_t v) {
-    assert(v);
-    return __builtin_clzll(v) ^ 63;
+	assert(v);
+	return __builtin_clzll(v) ^ 63;
 }
 
 int lsbpop(uint64_t *v) {
-    int b = lsb(*v);
-    *v &= *v - 1;
-    return b;
+	int b = lsb(*v);
+	*v &= *v - 1;
+	return b;
 }
 
 int msbpop(uint64_t *v) {
-    int b = msb(*v);
-    *v ^= 1ull << b;
-    return b;
+	int b = msb(*v);
+	*v ^= 1ull << b;
+	return b;
 }
 
 bool multiple(uint64_t v) {
-    return (v & (v - 1)) != 0;
+	return (v & (v - 1)) != 0;
 }
--- src/misc.c
+++ src/misc.c
@@ -5,28 +5,28 @@
 #include "search/tt.h"
 
 void memswap(void *restrict buffer, void *l, void *r, size_t size) {
-    memcpy(buffer, l, size);
-    memmove(l, r, size);
-    memcpy(r, buffer, size);
+	memcpy(buffer, l, size);
+	memmove(l, r, size);
+	memcpy(r, buffer, size);
 }
 
 #ifdef WIN32
 #include <windows.h>
 uint64_t time_ms() {
-    return GetTickCount();
+	return GetTickCount();
 }
 #else
 #include <sys/time.h>
 uint64_t time_ms() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 #endif
 
 void init(void) {
-    init_masks();
-    init_zobrist();
-    init_values();
-    init_tt();
+	init_masks();
+	init_zobrist();
+	init_values();
+	init_tt();
 }
