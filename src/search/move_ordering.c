ca328bcdc74146c867f9183c6d30f4aee43a8aee
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-10-02 07:08:48 +0200

    refactor: unify move ordering

--- src/search/move_ordering.c
+++ src/search/move_ordering.c
@@ -3,26 +3,18 @@
 #include "chess/position.h"
 #include "misc.h"
 
-void pick_move(Move moves[MAX_MOVES], unsigned int scores[MAX_MOVES], unsigned int count,
-              unsigned int start) {
-    for (unsigned int i = start + 1; i < count; i++) {
-        if (scores[i] > scores[start]) {
-            swap(&scores[i], &scores[start]);
-            swap(&moves[i], &moves[start]);
-        }
-    }
-}
-
-const int MVV_VALUES[] = {10, 20, 30, 40, 50, 90, 0, 0};
-
-void MVV(const Position *pos, Move moves[MAX_MOVES], unsigned int scores[MAX_MOVES],
-            unsigned int count) {
-    for (unsigned int i = 0; i < count; i++) {
-        const Move move = moves[i];
-        const Square to = move_to(move);
-        const Piece target = square_piece(pos, to);
-        scores[i] += MVV_VALUES[target];
-    }
+void pick_move(
+	Move moves[MAX_MOVES],
+	unsigned int scores[MAX_MOVES],
+	unsigned int count,
+        unsigned int start
+) {
+	for (unsigned int i = start + 1; i < count; i++) {
+		if (scores[i] > scores[start]) {
+			swap(&scores[i], &scores[start]);
+			swap(&moves[i], &moves[start]);
+		}
+	}
 }
 
 const int MVVLVA_VALUES[] = {
@@ -34,26 +26,29 @@ const int MVVLVA_VALUES[] = {
 	10, 20, 30, 40, 50, 90, 0, 0
 };
 
-void MVVLVA(const Position *pos, Move moves[MAX_MOVES], unsigned int scores[MAX_MOVES],
-            unsigned int count) {
-    for (unsigned int i = 0; i < count; i++) {
-        const Move move    = moves[i];
-        const Square from  = move_from(move);
-        const Square to    = move_to(move);
-        const Piece piece = square_piece(pos, from);
-        const Piece target = square_piece(pos, to);
-        scores[i] += MVVLVA_VALUES[piece * 8 + target];
-    }
-}
+void order(
+	Move moves[MAX_MOVES],
+	unsigned int scores[MAX_MOVES],
+	unsigned int count,
+	const Position *pos,
+	Move tt_move
+) {
+	for (unsigned int i = 0; i < count; i++) {
+		const Move     move   = moves[i];
+		const MoveType type   = move_type(moves[i]);
+		const Square   from   = move_from(move);
+		const Square   to     = move_to(move);
+		const Piece    piece  = square_piece(pos, from);
+		const Piece    target = square_piece(pos, to);
+		
+		if (moves[i] == tt_move) {
+			scores[i] += 1000;
+			continue;
+		}
+		
+		if (type == QPromotion)
+			scores[i] += 100;
 
-void order(Move tt_move, Move moves[MAX_MOVES], unsigned int scores[MAX_MOVES], unsigned int count) {
-    for (unsigned int i = 0; i < count; i++) {
-        if (moves[i] == tt_move) {
-            scores[i] += 1000;
-            continue;
-        }
-        const MoveType type = move_type(moves[i]);
-        if (type == QPromotion)
-            scores[i] += 100;
-    }
+		scores[i] += MVVLVA_VALUES[piece * 8 + target];
+	}
 }
